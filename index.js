d63fae0501b9d5f42191a120e215810ee4b8acdb
MelchiorForster <134959951+MelchiorForster@users.noreply.github.com>
2024-07-20 01:21:16 +0200

    weitere Buttons

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-
 let count = 0;
 
 const incButton = document.getElementById("incBtn");
 const decButton = document.getElementById("decBtn");
 const resButton = document.getElementById("rstBtn");
-const counter = document.getElementById("countLabel")
+const inc2Button = document.getElementById("incBtn2");
+const dec2Button = document.getElementById("decBtn2");
+const counter = document.getElementById("countLabel");
 
-incButton.onclick = function(){
-    count++;
-    counter.textContent=count;
-}
+incButton.onclick = function () {
+  count++;
+  counter.textContent = count;
+};
 
-decButton.onclick= function(){
-    count++;
-    counter.textContent=count;
-}
+decButton.onclick = function () {
+  count++;
+  counter.textContent = count;
+};
 
-resButton.onclick = function(){
-    count=0;
-    counter.textContent=count;
-}
+resButton.onclick = function () {
+  count = 0;
+  counter.textContent = count;
+};
+
+inc2Button.onclick = function () {
+  count = count + 2;
+  counter.textContent = count;
+};
+dec2Button.onclick = function () {
+  count = count - 2;
+  counter.textContent = count;
+};
